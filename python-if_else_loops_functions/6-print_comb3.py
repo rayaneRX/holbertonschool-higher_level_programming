60f78735d2b2edcd4968ab367d7d0c7d481f171e
rayaneRX <elkerayane@gmail.com>
2023-01-19 14:20:09 +0100

    add 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number1 in range(10):
+    for number2 in range(number1 + 1, 10):
+        if number1 == 8 and number2 == 9:
+            print("{}{}\n".format(number1, number2), end="")
+        else:
+            print("{}{}".format(number1, number2), end=", ")
