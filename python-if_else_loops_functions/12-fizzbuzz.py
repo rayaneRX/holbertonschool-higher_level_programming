0be0fa63e038bdee3fe5f1e91402b5aa30d44d0c
rayaneRX <elkerayane@gmail.com>
2023-01-19 15:04:37 +0100

    fizz buzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for s in range(1, 101):
+    if (s % 3 == 0 and s % 5 == 0):
+        print("FizzBuzz", end =' ')
+    elif (s % 3 == 0):
+        print("Fizz", end =' ')
+    elif (s % 5 == 0):
+        print("Buzz", end =' ')
+    else:
+        print(s, end =' ')
