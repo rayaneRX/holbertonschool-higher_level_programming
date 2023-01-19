96c55a41a33422efc4620f7540d93c71a98da8a0
rayaneRX <elkerayane@gmail.com>
2023-01-19 15:08:01 +0100

    fizz buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-for s in range(1, 101):
-    if (s % 3 == 0 and s % 5 == 0):
-        print("FizzBuzz", end =' ')
-    elif (s % 3 == 0):
-        print("Fizz", end =' ')
-    elif (s % 5 == 0):
-        print("Buzz", end =' ')
-    else:
-        print(s, end =' ')
+
+
+def fizzbuzz():
+    for s in range(1, 101):
+        if (s % 3 == 0 and s % 5 == 0):
+            print("FizzBuzz", end=' ')
+        elif (s % 3 == 0):
+            print("Fizz", end=' ')
+        elif (s % 5 == 0):
+            print("Buzz", end=' ')
+        else:
+            print(s, end=' ')
