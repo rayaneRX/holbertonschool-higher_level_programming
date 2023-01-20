feb25ba21f0aad7101ad9cbd08cc425c32a52758
rayaneRX <elkerayane@gmail.com>
2023-01-20 17:13:22 +0100

    add the 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) - 1 == 0:
+        print("{} arguments.".format(len(argv) - 1))
+    if len(argv) - 1 == 1:
+        print("{} argument:".format(len(argv) - 1))
+    if len(argv) - 1 > 1:
+        print("{} arguments:".format(len(argv) - 1))
+    if len(argv) - 1 > 0:
+        for i in range (1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+        
