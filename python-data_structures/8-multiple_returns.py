8d16ba2c914b82c9778d397f9bec3251c7f321f1
rayaneRX <elkerayane@gmail.com>
2023-01-24 15:31:11 +0100

    ad the 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    #first_c = sentence[0]
     if sentence == "":
-        #first_c = None
         return (len(sentence), None)
     else:
         return (len(sentence), sentence[0])
