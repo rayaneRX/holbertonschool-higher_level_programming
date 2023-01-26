8a4f508c252012168fc44ef13c5c9eb3aed23a58
rayaneRX <elkerayane@gmail.com>
2023-01-26 14:44:47 +0100

    add the 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    newlist = []
+    for i in my_list:
+        if i == search:
+            newlist.append(replace)
+        else:
+            newlist.append(i)
+            
+    return newlist
+
+'''
+return list(replace if i == search else i for i in my_list)
+'''
