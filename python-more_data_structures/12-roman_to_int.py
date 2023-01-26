ce6dc4f3b3fbd4526c1a51fdf814603bc662d30f
rayaneRX <elkerayane@gmail.com>
2023-01-26 17:27:50 +0100

    add th e12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,17 +3,17 @@
 
 def roman_to_int(roman_string):
     roman = {
-        'I':1,
-        'V':5,
-        'X':10,
-        'L':50,
-        'C':100,
-        'D':500,
-        'M':1000
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
     if type(roman_string) is not str or roman_string is None:
         return 0
-    res = 0 
+    res = 0
     prv = 0
     roman_list = list(roman_string)
     for i in roman_list:
