a61cd3d67e712df14b3bd204a62eda3df3059fef
rayaneRX <elkerayane@gmail.com>
2023-01-31 03:33:38 +0100

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square that defines a square """
+
+
+class Square:
+    """cPrivate instance attribute: size Instantiation with optional """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
