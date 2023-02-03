4cbf1d9e6a0b1dd222f739ed80b78ba05ed28222
rayaneRX <elkerayane@gmail.com>
2023-02-03 18:00:37 +0100

    add the 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """instance attribute: width"""
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
