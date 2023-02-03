0bc0f124c1114daaeba12d804752a6bd78a12c70
rayaneRX <elkerayane@gmail.com>
2023-02-03 16:59:00 +0100

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -9,8 +9,8 @@ class Square:
     Public instance method: def my_print(self):
     that prints in stdout the square with the character #"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
