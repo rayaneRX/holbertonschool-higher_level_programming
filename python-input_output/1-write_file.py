58f39de4749fb2ce32837cf748c748adb098648c
rayaneRX <elkerayane@gmail.com>
2023-02-08 01:06:45 +0100

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Write a function that writes a string to a text file (UTF8)"""
 
+
 def write_file(filename="", text=""):
     """returns the number of characters written"""
     with open(filename, 'w', encoding='utf-8') as f:
