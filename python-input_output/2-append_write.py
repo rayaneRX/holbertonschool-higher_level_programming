bb76f261f9f3f8ff24daf56393dc26eaa02f0e97
rayaneRX <elkerayane@gmail.com>
2023-02-08 01:46:22 +0100

    add the a

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,6 +6,6 @@
 def append_write(filename="", text=""):
     """function that appends a string at the end of a text file (UTF8)
     and returns the number of characters added:"""
-    with open(filename, "a") as file:
-        file.write(text)
+    with open(filename, "a") as f:
+        f.write(text)
     return len(text)
