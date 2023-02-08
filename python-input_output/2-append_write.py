dada557ef731039ade4e42b49b0c66e2f04f3b89
rayaneRX <elkerayane@gmail.com>
2023-02-08 01:41:59 +0100

    add the a

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,6 +4,7 @@
 
 def write_file(filename="", text=""):
     """returns the number of characters written"""
+
     with open(filename, 'a', encoding='utf-8') as f:
         f.write(text)
     return len(text)
