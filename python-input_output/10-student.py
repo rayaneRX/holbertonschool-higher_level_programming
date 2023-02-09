a703ccd2eb5c169461c724e3659d4b886ff2e7be
rayaneRX <elkerayane@gmail.com>
2023-02-09 04:18:39 +0100

    ^^

---
+++ python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
