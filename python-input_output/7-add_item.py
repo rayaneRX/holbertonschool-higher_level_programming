87dfbda799801a3845a48846f5a77a5bd0eab7b3
rayaneRX <elkerayane@gmail.com>
2023-02-09 03:39:24 +0100

    add teh 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,20 +3,17 @@
 and then save them to a file:"""
 
 
-
 import sys
 import json
-
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item(*args):
-    try:
-        items = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        items = []
+try:
+    items = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    items = []
 
-    save_to_json_file(item + sys.argv[1:], "add_item.json")
+save_to_json_file(items + sys.argv[1:], "add_item.json")
