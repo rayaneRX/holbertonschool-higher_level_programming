89190ed77c43aae64f0dae60d450758877571a0c
rayaneRX <elkerayane@gmail.com>
2023-02-15 01:28:43 +0100

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ class Rectangle that inherits from Base"""
-
 Base = __import__('models').Base
 
 
 class Rectangle(Base):
-    """Private instance attributes,each with its own public getter and setter:
+    """Private instance attributes,each with its own public getter and setter
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
