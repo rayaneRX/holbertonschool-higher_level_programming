39a031814d8d41571f89d46587f6f6d488e4f4d6
rayaneRX <elkerayane@gmail.com>
2023-02-16 13:51:42 +0100

    add the sq

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,6 +21,18 @@ class Square(Rectangle):
         s = self.size
         return f"[Square] ({id}) {x}/{y} - {s}"
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = self.height = value
+
     def update(self, *args, **kwargs):
         """that assigns an argument to each attribute"""
         if args:
