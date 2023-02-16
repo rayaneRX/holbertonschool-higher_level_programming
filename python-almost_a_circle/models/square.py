a15b99bdba8011abfa5c1a3f530af728c89a4314
rayaneRX <elkerayane@gmail.com>
2023-02-16 04:09:20 +0100

    add the square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,3 +35,8 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+    
+    def to_dictionary(self):
+        """eturns the dictionary representation of a Rectangle"""
+
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
