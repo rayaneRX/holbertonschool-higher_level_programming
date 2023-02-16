534fe101ff14535c0e08265760c5db83bd4021e1
rayaneRX <elkerayane@gmail.com>
2023-02-16 03:56:55 +0100

    add the square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,3 +20,18 @@ class Square(Rectangle):
         y = self.y
         s = self.size
         return f"[Square] ({id}) {x}/{y} - {s}"
+
+    def update(self, *args, **kwargs):
+        """that assigns an argument to each attribute"""
+        if args:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.size = args[1]
+            if len(args) == 3:
+                self.x = args[2]
+            if len(args) == 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
