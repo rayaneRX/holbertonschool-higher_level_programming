84a1f33a5b13cb488fdf74b0127593a337f1957d
rayaneRX <elkerayane@gmail.com>
2023-02-16 14:24:35 +0100

    add the base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """eturns the JSON string representation of list_dictionaries"""
+        """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == {}:
             return "[]"
         else:
@@ -34,8 +34,19 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """eturns the list of the JSON string representation"""
+        """returns the list of the JSON string representation"""
         if json_string is None or json_string == {}:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set:"""
+        if cls.__name__ == "Rectangle":
+            instance = cls(8, 12)
+        else:
+            instance = cls(8)
+
+        instance.update(**dictionary)
+        return instance
