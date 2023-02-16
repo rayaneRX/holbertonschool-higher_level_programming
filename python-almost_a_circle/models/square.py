e6cb50c5fd6e744a16777faa7658989d31f38eac
rayaneRX <elkerayane@gmail.com>
2023-02-16 13:55:30 +0100

    add the sq

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -36,19 +36,19 @@ class Square(Rectangle):
     def update(self, *args, **kwargs):
         """that assigns an argument to each attribute"""
         if args:
-            if len(args) == 1:
+            if len(args) >= 1:
                 self.id = args[0]
-            if len(args) == 2:
+            if len(args) >= 2:
                 self.size = args[1]
-            if len(args) == 3:
+            if len(args) >= 3:
                 self.x = args[2]
-            if len(args) == 4:
+            if len(args) >= 4:
                 self.y = args[3]
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """eturns the dictionary representation of a Rectangle"""
+        """returns the dictionary representation of a Rectangle"""
 
         return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
