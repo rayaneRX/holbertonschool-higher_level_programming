33bdf453c5119f016c3eb36bda6069e18d2b3e64
rayaneRX <elkerayane@gmail.com>
2023-02-16 14:10:19 +0100

    add the base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,6 +21,7 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs"""
