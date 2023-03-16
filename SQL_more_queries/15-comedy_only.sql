fcf0590798750a37104531d25ce61aee32c6860f
rayaneRX <elkerayane@gmail.com>
2023-03-16 01:49:57 +0100

    add the 16

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_genres.name FROM tv_genres
+SELECT tv_shows.title FROM tv_shows
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name = "Comedy"
