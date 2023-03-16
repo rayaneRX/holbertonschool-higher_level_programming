e9ae761737603692522686cd01dfac81950d170d
rayaneRX <elkerayan@gmail.com>
2023-03-16 17:38:39 +0100

    lists whit N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY name")
 
     result = cursor.fetchall()
 
