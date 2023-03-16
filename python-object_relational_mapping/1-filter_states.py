f5c81f8be7c0ea5b9093bd899b8784a8a1d5cb1e
rayaneRX <elkerayan@gmail.com>
2023-03-16 17:32:12 +0100

    lists whit N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
