fe4c440b823b32935c98a4e65f39846665d3db4c
rayaneRX <elkerayan@gmail.com>
2023-03-17 17:17:29 +0100

    add the 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,8 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], host='localhost', port=3306,)
 
     cursor = db.cursor()
 
