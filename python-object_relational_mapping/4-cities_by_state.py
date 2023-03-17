2c0f26fbf396df719f51fa37cb88808a4a49b599
rayaneRX <elkerayan@gmail.com>
2023-03-17 16:27:52 +0100

    lists all cities

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("SELECT cities.id, cities.name , states.name\
-                   FROM cities JOIN states ON states.id = cities.state_id ORDER BY cities.id;")
+                   FROM cities JOIN states ON states.id = cities.state_id\
+                   ORDER BY cities.id;")
 
     result = cursor.fetchall()
 
