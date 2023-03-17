dd953ddb8c6740eb3cb20da626524df30946029d
rayaneRX <elkerayan@gmail.com>
2023-03-17 19:45:50 +0100

    7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from sqlalchemy import (create_engine)
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
