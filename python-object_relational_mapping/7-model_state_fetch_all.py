bdd0e6bb3806a6beb47920a418e0654029118bba
rayaneRX <elkerayane@gmail.com>
2023-03-17 20:12:52 +0100

    add the

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,8 +2,9 @@
 """script that lists all State objects from the database hbtn_0e_6_usa"""
 
 import sys
-from model_state import State, Base
+from model_state import Base, State
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
