8036f928e59aa7123b4d28a029dd326ff8d42e96
rayaneRX <elkerayane@gmail.com>
2023-04-13 01:33:33 +0200

    foctorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial(num) {
+  if (isNaN(num) || num === 0) {
+    return 1;
+    } else {
+    return num * factorial(num - 1);
+  }
+}
+  
+const arg = process.argv[2];
+
+console.log(factorial(parseInt(arg)));
+  
