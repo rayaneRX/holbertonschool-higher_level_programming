9f2dbc0c4f020d273759d283a9f4b32dac8132dc
rayaneRX <elkerayane@gmail.com>
2023-04-12 07:21:18 +0200

    add a is in the 2

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@
 if (process.argv[2] && process.argv[3]) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else if (process.argv[2]) {
-    console.log(process.argv[2] + ' is undefined')
+  console.log(process.argv[2] + ' is undefined')
 } else {
   console.log('undefined is undefined');
 }
