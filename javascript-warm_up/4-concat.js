60b978872c1ac57845f377ce4424662e1c33711e
rayaneRX <elkerayane@gmail.com>
2023-04-12 07:13:03 +0200

    add a is in the 2

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,5 +3,5 @@
 if (process.argv[2] && process.argv[3]) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else {
-  console.log('undefined is undefined');
+  console.log('HBTN is undefined');
 }
