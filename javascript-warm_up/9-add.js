b383258badffe7603f545173e07c6b2141c5a9f9
rayaneRX <elkerayane@gmail.com>
2023-04-13 15:40:01 +0200

    fix

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
- function add (a, b) {
+function add (a, b) {
   return parseInt(a) + parseInt(b);
-    }
-  
-  const a = process.argv[2];
-  const b = process.argv[3];
+}
 
-  console.log(add(a, b));
+const a = process.argv[2];
+const b = process.argv[3];
+
+console.log(add(a, b));
