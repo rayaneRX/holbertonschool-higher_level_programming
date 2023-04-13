b73e98a48227f1dd2a1333b2b1c82bbbff11f09e
rayaneRX <elkerayane@gmail.com>
2023-04-13 16:00:40 +0200

    fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-function factorial(num) {
-    if (isNaN(num) || num === 0) {
-      return 1;
-    } else {
-      return num * factorial(num - 1);
-    }
+function factorial (num) {
+  if (isNaN(num) || num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
   }
+}
 
-  const arg = process.argv[2];
+const arg = process.argv[2];
 
-  console.log(factorial(parseInt(arg)));
+console.log(factorial(parseInt(arg)));
