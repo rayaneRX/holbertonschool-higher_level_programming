74608f8f60ae8717bd364bc6024a82a1512b87fd
rayaneRX <elkerayane@gmail.com>
2023-04-14 13:00:31 +0200

    fix

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
+
 const oldSquare = require('./5-square.js');
 
-class Square extends Square {
-    charPrint (c) {
-        if (c === undefined) {
-          c = 'X';
-        }
+class Square extends oldSquare {
+  charPrint (c) {
+    if (c) {
+      let line = '';
       for (let i = 0; i < this.width; i++) {
-        let row = '';
-        for (let j = 0; j < this.height ; j++) {
-          row += c;
-        }
-        console.log(row);
+        line += c;
       }
-    }
+      for (let i = 0; i < this.height; i++) {
+        console.log(line);
+      }
+    } else this.print();
   }
+}
 module.exports = Square;
