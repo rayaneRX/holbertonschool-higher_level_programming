4b7d47cd8de6fa003fc0b24cdad6397c49574177
rayaneRX <elkerayane@gmail.com>
2023-04-14 03:49:43 +0200

    add the task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,7 @@
 class Rectangle {
     constructor (w, h) {
       if (w <= 0 || h <= 0) {
-        this.width = undefined;
-        this.height = undefined;
+        return;
       } else {
       this.width = w;
       this.height = h;
