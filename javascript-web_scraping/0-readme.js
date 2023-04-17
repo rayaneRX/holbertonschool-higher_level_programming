b0532e8202088265f64e2503d64ffa8dbd256051
rayaneRX <elkerayane@gmail.com>
2023-04-17 15:14:50 +0200

    reads and prints the content of a file.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const ftr = process.argv[2];
+
+fs.readFile(ftr, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
