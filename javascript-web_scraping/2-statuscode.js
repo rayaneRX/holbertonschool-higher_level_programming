ce51e740329041a42812f7ac4ceab9f47461420c
rayaneRX <elkerayane@gmail.com>
2023-04-18 03:13:24 +0200

    display the status code of a GET

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('code: ' + response.statusCode);
+});
