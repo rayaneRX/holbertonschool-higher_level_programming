eb4eff38e16bf4b8f2e54de64726589e83a14446
rayaneRX <elkerayane@gmail.com>
2023-04-18 18:19:50 +0200

    gets the contents of a webpage and stores it in a file.

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+const ftr = process.argv[2];
+const str = process.argv[3];
+
+request(ftr, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  fs.writeFile(str, body, 'utf-8', function (error)  {
+    if (error) {
+    console.error(error);
+    }
+  });
+});
