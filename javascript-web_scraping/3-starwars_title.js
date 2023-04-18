d32c6b89c91130c9ea5624b6ff7729dad7d1f84e
rayaneRX <elkerayane@gmail.com>
2023-04-18 15:53:42 +0200

    fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 
 const movieID = process.argv[2];
-const api = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+const api = "https://swapi-api.hbtn.io/api/films/" + movieID;
 
-request(api, function(error, response, body) {
+request(api, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
