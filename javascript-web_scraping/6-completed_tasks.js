3f8d42574d22fe2d3bd4fb0ff090bb9136e0347c
rayaneRX <elkerayane@gmail.com>
2023-04-19 14:08:04 +0200

    fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,18 +7,16 @@ request(api, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
+  } const todo = JSON.parse(body);
+  const completedTasks = {};
+  const completed = [];
+  for (const todos of todo) {
+    if (todos.completed === true) completed.push(todos);
   }
-    const todo = JSON.parse(body);
-    const completedTasks = {};
-    const completed = []
-    for (const todos of todo) {
-          if (todos.completed === true)
-            completed.push(todos)}
-    for (let i=0; i < completed.length; i++) {
+  for (let i = 0; i < completed.length; i++) {
     const userId = completed[i].userId;
     if (completedTasks[userId]) completedTasks[userId]++;
     else completedTasks[userId] = 1;
-
   }
   console.log(completedTasks);
 });
